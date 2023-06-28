d506caa660ce8707eb18295cd26fd7fbb7108bed
никита <23018519+nikita@users.noreply.github.com>
2023-06-27 18:08:30 -0700

    chore: add ubuntu 23 support

--- tools/ubuntu_setup.sh
+++ tools/ubuntu_setup.sh
@@ -110,6 +110,9 @@ function install_ubuntu_focal_requirements() {
 if [ -f "/etc/os-release" ]; then
   source /etc/os-release
   case "$VERSION_CODENAME" in
+    "lunar")
+      install_ubuntu_lts_latest_requirements
+      ;;
     "jammy")
       install_ubuntu_lts_latest_requirements
       ;;
